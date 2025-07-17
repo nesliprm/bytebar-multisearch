112a252716de47a5fff170e0a45195a6b60963ab
Nesli <nesliparma@gmail.com>
2025-07-17 22:26:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -149,7 +149,8 @@ function generateAiCocktail(event) {
   showBottomPanel();
   showTypewriterLoading();
 
-  let apiKey = API_KEY;
+  let apiKey =
+    "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";
 
   const messages = [
     {
